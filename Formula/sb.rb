2e12b87c3b1f6388822631711b465203a50672d0
Masoko <jeleff@gmail.com>
2026-06-07 15:41:07 +0300

    brew: update sb to v0.6.18

--- Formula/sb.rb
+++ Formula/sb.rb
@@ -1,8 +1,8 @@
 class Sb < Formula
   desc "Simple terminal file manager"
   homepage "https://github.com/hjelev/sb"
-  url "https://github.com/hjelev/sb/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  url "https://github.com/hjelev/sb/archive/refs/tags/v0.6.18.tar.gz"
+  sha256 "c001ee007b13b3f15d043bb7638685d899bd6d6ed71013529936e634eec8c380"
   license "MIT" # Update if you use a different license
 
   depends_on "rust" => :build
